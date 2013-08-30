09b949597a8a046c3296506a60b934ef2e991101
Christoffer Pettersson <christoffer.pettersson@qlikview.com>
2013-08-30 09:38:59 +0200

    more test cases concerning error-util

--- src/utils/error-util.js
+++ src/utils/error-util.js
@@ -4,6 +4,10 @@
 
 exports.create = function ( message, data ) {
 
+	if ( !message ) {
+		throw new Error( "Invalid message." );
+	}
+
 	var error = new Error( message );
 
 	for ( var property in data ) {
--- tests/utils/error-util-test.js
+++ tests/utils/error-util-test.js
@@ -8,11 +8,24 @@ var errorUtil = require( "../../src/utils/error-util.js" );
  * Tests
  */
 
-exports.testMessage = function ( test ) {
+exports.testIsErrorObject = function ( test ) {
 
 	var error = errorUtil.create( "This is the message" );
 
-	test.equal( error.message, "This is the message" );
+	test.equal( typeof error, "object" );
+	test.equal( error.name, "Error" );
+
+	test.expect( 2 );
+	test.done();
+
+}
+
+exports.testMessage = function ( test ) {
+
+	var message = "This is the message";
+	var error = errorUtil.create( message );
+
+	test.equal( error.message, message );
 
 	test.expect( 1 );
 	test.done();
@@ -21,21 +34,41 @@ exports.testMessage = function ( test ) {
 
 exports.testData = function ( test ) {
 
+	var message = "This is the message";
 	var file = "c:\\source.js";
 	var object = {
 		path: "c:\\folder\\"
 	}
 
-	var error = errorUtil.create( "This is the message", {
+	var error = errorUtil.create( message, {
 		file: file,
 		object: object
 	} );
 
-	test.equal( error.message, "This is the message" );
+	test.equal( error.message, message );
 	test.equal( error.file, file );
 	test.equal( error.object, object );
 
 	test.expect( 3 );
+	test.done();
+
+}
+
+exports.testEmptyMessageIsInvalid = function ( test ) {
+
+	try {
+		errorUtil.create();
+	} catch ( error ) {
+		test.equal( error.message, "Invalid message." );
+	}
+
+	try {
+		errorUtil.create( undefined );
+	} catch ( error ) {
+		test.equal( error.message, "Invalid message." );
+	}
+
+	test.expect( 2 );
 	test.done();
 
 }
